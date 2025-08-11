2c8e1e221685629acc3485dac6c33ea616cc4763
Robson Lima <seducedbywine@gmail.com>
2025-08-10 21:05:37 -0300

    fix/desabilitar-cors-localhost-e-acionar-prisma-generate

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // Habilita o CORS para o frontend
-  app.enableCors({
-    origin: 'http://localhost:3000', // Permite apenas seu frontend
-    credentials: true, // Se estiver usando cookies ou headers como Authorization
-  });
-  
+  const app = await NestFactory.create(AppModule);  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
