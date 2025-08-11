d6247df7c3f9b2e4bb49df01dd2dcf8dac89f8b3
Robson Lima <seducedbywine@gmail.com>
2025-08-10 21:31:53 -0300

    fix/atualizacao-do-main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import './vercel-prisma-fix';
+import "../vercel.json";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);  
