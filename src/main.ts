e91a629c3c01373eebfae3760b544af6d1378f65
Robson Lima <seducedbywine@gmail.com>
2025-08-10 21:20:22 -0300

    chore/atualizacao-de-configuracao-prisma-neon

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import './vercel-prisma-fix';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);  
