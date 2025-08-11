b32d61a683b1804ed4c0d9d5dd94403286352a18
Robson Lima <seducedbywine@gmail.com>
2025-08-10 22:26:08 -0300

    fix/mudanca-de-arquivos-para-render

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import "../vercel.json";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0'); 
-  console.log(`🚀 Running on port ${port}`);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
