b1f8a0cae7d3a5582cb74c378b04207f922874c4
Robson Lima <seducedbywine@gmail.com>
2025-08-11 00:47:11 -0300

    fix/permissao-para-app-url

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   app.enableCors({
+    origin: process.env.APP_URL, 
+    credentials: true, 
+  });
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
